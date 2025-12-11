06defbaa8e4d43e626e76a937beadc634e1ea8ae
Jonatan Xavier <j.b.xavier09@gmail.com>
2025-12-11 13:16:37 -0300

    revisao relations

--- app/models/relations.js
+++ app/models/relations.js
@@ -2,21 +2,22 @@
 //porem cada relacionamento pode ser implementado dentro de sua model separadamente
 module.exports = function (models) {
   // Relacionamento: Um Cliente pode ter muitos Aluguéis
-  models.Cliente.hasMany(models.Aluguel, {
-    foreignKey: 'clienteId',
+  models.Cliente.hasMany(models.Aluguel, { //UM CLIENTE PRA MUITOS ALUGEIS
+    foreignKey: 'clienteId', // chave estrangeira em Aluguel CRIE ESSA COLUNA NO BANCO
     as: 'alugueis', // Alias para facilitar as consultas
   });
-  models.Aluguel.belongsTo(models.Cliente, {
-    foreignKey: 'clienteId',
+  models.Aluguel.belongsTo(models.Cliente, {//  UM ALUGEL PERTENCE A UM CLIENTE
+    foreignKey: 'clienteId', // chave estrangeira em Aluguel CRIE ESSA COLUNA NO BANCO
     as: 'cliente',
   });
 
   // Relacionamento: Um Filme pode estar em muitos Aluguéis
-  models.Filme.hasMany(models.Aluguel, {
+  models.Filme.hasMany(models.Aluguel, { // UM FILME PRA MUITOS ALUGEIS
     foreignKey: 'filmeId',
+    as: 'alugueis', // Alias para facilitar as consultas
   });
-  models.Aluguel.belongsTo(models.Filme, {
-    foreignKey: 'filmeId',
-    as: 'filme',
+  models.Aluguel.belongsTo(models.Filme, { // UM ALUGEL PERTENCE A UM FILME
+    foreignKey: 'filmeId', // chave estrangeira em Aluguel CRIE ESSA COLUNA NO BANCO
+    as: 'filme', // Alias para facilitar as consultas
   });
 };
